0b2a2fa11d16481d07a9a9e9daa0d4ab053eb283
Junichi Ito <jit@sonicgarden.jp>
2023-08-19 18:54:42 +0900

    newでモーダルが開くところまで

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+import { Modal } from "bootstrap"
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  connect() {
+    this.modal = new Modal(this.element)
+    this.modal.show()
+  }
+}
