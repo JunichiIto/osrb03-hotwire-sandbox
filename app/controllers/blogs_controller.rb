fc4c618b9fcda35903c702591008f9eba3adcf08
Junichi Ito <jit@sonicgarden.jp>
2023-08-19 17:07:19 +0900

    Add content to Blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -53,6 +53,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title)
+      params.require(:blog).permit(:title, :content)
     end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
+  has_rich_text :content
+
   validates :title, presence: true
 end
