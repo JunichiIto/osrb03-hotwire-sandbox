c2dd7b75af2b790fcce0245a53c41eb0e7d0a5d1
Junichi Ito <jit@sonicgarden.jp>
2023-09-02 14:42:26 +0900

    Remove unused method / Use prepend instead of update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def turbo_stream_flash
-    turbo_stream.append "flashes", partial: "flash"
-  end
-
   def icon_with_text(icon_name, text)
     tag.span(icon(icon_name), class: "me-2") + tag.span(text)
   end
